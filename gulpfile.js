aa0f12d58b68e88770e27a46441d5411e853db60
Section9Code <andrew@section9.co>
2016-12-15 12:05:42 +0000

    Added Gulp build help to the project

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// include gulp
+var gulp = require('gulp'); 
+
+// include plug-ins
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var minifyCSS = require('gulp-minify-css');
+var runSequence = require('run-sequence');
+var del = require('del');
+
+gulp.task('build-clean', function() {
+  return del(['./assets/js/site*.js','./assets/css/site.css'])
+});
+
+gulp.task('build-scripts', function() {
+  return gulp.src('./assets/js/*.js')
+    .pipe(concat('site.js'))
+    .pipe(minify())
+    .pipe(gulp.dest('./assets/js'))
+});
+
+gulp.task('build-styles', function() {
+  return gulp.src('./assets/css/*.css')
+    .pipe(concat('site.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./assets/css'))
+});
+
+gulp.task('build', function(callback) {
+  runSequence('build-clean',
+              ['build-scripts', 'build-styles'],
+              callback);
+});
